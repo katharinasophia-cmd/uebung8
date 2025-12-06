4ece53cbe6837b22f214f124f10c4be240a23a63
Katharina Raps <katharinaraps@28d4a3f9-b066-443d-af49-60f9b0956116.fritz.box>
2025-12-06 22:53:35 +0100

    erste HA

---
+++ src/h1/Bruch.java
@@ -0,0 +1,74 @@
+package h1;
+
+public class Bruch {
+	
+	//Attribute:
+	
+	int zaehler;
+	int nenner;
+
+	//Konstruktor:
+	
+	public Bruch (int zaehler, int nenner) {
+		
+		this.zaehler = zaehler;
+		this.nenner = nenner;
+	
+	}
+	
+	//Methoden:
+	
+	private int ggT (int x, int y) {
+		
+		x = Math.abs(x);
+		y = Math.abs(y);
+		
+		if (x<y) {
+			int temp_x = x;
+			x = y;
+			y = temp_x;
+		}
+		
+		int r = 1;
+		
+		do {
+			r = x % y;
+			
+			x = y;
+			y = r;
+		}
+		
+		while (r>0);
+		
+		return x;
+	}
+	
+	public void shorten () {
+		
+		int temp_zaehler = zaehler;
+		
+		zaehler = zaehler / ggT(zaehler,nenner);
+		nenner = nenner / ggT(temp_zaehler,nenner);
+		
+		System.out.println (zaehler + "/" + nenner);
+	}
+	
+	public boolean hasSameValueAs (Bruch b) {
+		
+		b.shorten();
+		
+		if (b.zaehler == zaehler && b.nenner == nenner) {
+			System.out.println("true");
+			return true;
+		}
+		else {
+			System.out.println("false");
+		return false;
+		}
+	}
+	
+	
+	
+	
+
+}
---
+++ src/h1/H1_main.java
@@ -0,0 +1,13 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args) {
+		
+		Bruch objekt1 = new Bruch (18,21);
+		Bruch b = new Bruch (8,7);
+		
+		objekt1.shorten();
+		objekt1.hasSameValueAs(b);
+	}
+
+}
